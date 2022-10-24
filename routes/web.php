4c2c81586ec7cf97b420a1e2dc44c2bfe79c9257
Febrian Fauzan Rachman <76427470+mpeb1502@users.noreply.github.com>
2022-10-24 15:04:22 +0700

    Menyelesaikan vidio ke 5 tentang Model, Collections & Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,59 +33,5 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Febrian Fauzan R",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sit iure aliquam maiores laudantium maxime inventore illum beatae aliquid distinctio minima, aut quidem aspernatur, error praesentium deserunt perferendis vitae quos eum odit quod magnam minus. Sint aut voluptatum iure eos illum repudiandae impedit corrupti reprehenderit consectetur! Magnam, vero optio. Voluptatibus molestias nostrum, perferendis corporis quo cum quaerat. Repellendus, quos. Provident corrupti repudiandae, cum eos earum esse dolore sunt nostrum non 
-            perspiciatis unde possimus asperiores doloribus, cupiditate aliquam, 
-            sed mollitia dolor? Voluptatibus cum impedit rem accusantium maxime soluta est sed ad?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ikhsan Rachmat",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sit iure aliquam maiores laudantium maxime inventore illum beatae aliquid distinctio minima, aut quidem aspernatur, error praesentium deserunt perferendis vitae quos eum odit quod magnam minus. Sint aut voluptatum iure eos illum repudiandae impedit corrupti reprehenderit consectetur! Magnam, vero optio. Voluptatibus molestias nostrum, perferendis corporis quo cum quaerat. Repellendus, quos. Provident corrupti repudiandae, cum eos earum esse dolore sunt nostrum non 
-            perspiciatis unde possimus asperiores doloribus, cupiditate aliquam"
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// Halaman singel Post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Febrian Fauzan R",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sit iure aliquam maiores laudantium maxime inventore illum beatae aliquid distinctio minima, aut quidem aspernatur, error praesentium deserunt perferendis vitae quos eum odit quod magnam minus. Sint aut voluptatum iure eos illum repudiandae impedit corrupti reprehenderit consectetur! Magnam, vero optio. Voluptatibus molestias nostrum, perferendis corporis quo cum quaerat. Repellendus, quos. Provident corrupti repudiandae, cum eos earum esse dolore sunt nostrum non 
-            perspiciatis unde possimus asperiores doloribus, cupiditate aliquam, 
-            sed mollitia dolor? Voluptatibus cum impedit rem accusantium maxime soluta est sed ad?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Ikhsan Rachmat",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius sit iure aliquam maiores laudantium maxime inventore illum beatae aliquid distinctio minima, aut quidem aspernatur, error praesentium deserunt perferendis vitae quos eum odit quod magnam minus. Sint aut voluptatum iure eos illum repudiandae impedit corrupti reprehenderit consectetur! Magnam, vero optio. Voluptatibus molestias nostrum, perferendis corporis quo cum quaerat. Repellendus, quos. Provident corrupti repudiandae, cum eos earum esse dolore sunt nostrum non 
-            perspiciatis unde possimus asperiores doloribus, cupiditate aliquam"
-        ]
-    ];
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if ($post["slug"] == $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Singel Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
