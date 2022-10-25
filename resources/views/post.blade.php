e646da172f638f145f79b1e8c1f6545ceab0ef66
Febrian <febrianfauzan860@gmail.com>
2022-10-25 23:55:57 +0700

    Menyelesaikan vidio ke 8 tentang Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,11 @@
 @extends ('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{$post->title}}</h1>
+    <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By. Febrian Fauzan Rachman in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
+
     <a href="/blog">Back to Posts</a>
 @endsection
-
-
