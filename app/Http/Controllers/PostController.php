8f1eb0178a64b384d48063654dda48b0da8ef117
Febrian <febrianfauzan860@gmail.com>
2022-10-26 12:18:23 +0700

    Menyelesaikan vidio ke 11 tentang N+1 Problem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,9 +10,9 @@ class PostController extends Controller
     public function index()
     {
         return view('posts', [
-            "title" => "Posts",
+            "title" => "All posts",
             // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => Post::with(['author', 'category'])->latest()->get()
         ]);
     }
 
