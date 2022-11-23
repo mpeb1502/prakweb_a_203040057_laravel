7f28462118937b353d0c01b0afd16163dc2dda77
Febrian <febrianfauzan860@gmail.com>
2022-11-23 19:25:22 +0700

    Menyelesaikan Vidio Ke 18 Tentang Create Post Form

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
-
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -62,5 +62,6 @@ Route::get('/dashboard', function () {
 })->middleware('auth');
 
 
-
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])
+    ->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
